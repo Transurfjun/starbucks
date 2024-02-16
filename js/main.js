057055ec6c895adce20cf37ffe4211ed1b07c174
Transurfjun <hsunnn333@gmail.com>
2024-02-16 20:41:01 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-// 통합검색창
-const searchEL = document.querySelector('.search');
-const searchInputEl = searchEL.querySelector('input');
-
-searchEL.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEL.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색'); 
-  // attribute(html 속성)를 지정해준다
-});
-
-  // blur 포커스 해제
-searchInputEl.addEventListener('blur', function () {
-  searchEL.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', ''); 
-});
 
 // 우측 뱃지
 const badgeEl = document.querySelector('header .badges');
@@ -191,6 +172,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-// footer
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
